54575daee621f0679df3b428c050a26c71ba34f2
Zeshawn Khan <zeshawnk@gmail.com>
2018-08-23 00:49:28 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@
 def speak_to_grandma(phrase)
    if phrase != phrase.upcase
      return "HUH?! SPEAK UP, SONNY!"
-  else if 
+  elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
     
      
